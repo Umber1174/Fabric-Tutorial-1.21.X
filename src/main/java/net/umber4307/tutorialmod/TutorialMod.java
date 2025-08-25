0a44c7b0dc9188f5a7e6a53e8281a5843d479166
Umber1174 <mi241338@edu.okinawa-ct.ac.jp>
2025-08-25 20:32:52 +0900

    アイテムのグループ分けをした

--- src/main/java/net/umber4307/tutorialmod/TutorialMod.java
+++ src/main/java/net/umber4307/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.umber4307.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.umber4307.tutorialmod.block.ModBlocks;
+import net.umber4307.tutorialmod.item.ModItemGroups;
 import net.umber4307.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
 	}
